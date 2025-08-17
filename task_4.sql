b03c5031e5601336d6b76d52647d5545d7aa6ec7
frederick <muhirefrederick@gmail.com>
2025-08-17 23:03:07 +0200

    file updated

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,16 @@
--- Switch to the correct database
+-- task_4.sql
+
 USE alx_book_store;
 
--- Query the metadata to get full description of 'Books' table
--- task_4.sql
-SHOW CREATE TABLE Books;
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Data Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_DEFAULT AS 'Default Value',
+    EXTRA AS 'Extra Info',
+    COLUMN_KEY AS 'Key'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
