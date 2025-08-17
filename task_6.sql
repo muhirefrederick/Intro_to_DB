90932b555f78f830a40567e383237cd9ada417e4
frederick <muhirefrederick@gmail.com>
2025-08-17 23:22:54 +0200

    new

--- task_6.sql
+++ task_6.sql
@@ -1,14 +1,7 @@
 -- task_6.sql
 
-USE alx_book_store;
-
-SET @sql = CONCAT(
-    'INSERT INTO ', 'customer', ' ',
-    'SELECT 2, ''Blessing Malik'', ''bmalik@sandtech.com'', ''124 Happiness  Ave.'' UNION ALL ',
-    'SELECT 3, ''Obed Ehoneah'', ''eobed@sandtech.com'', ''125 Happiness  Ave.'' UNION ALL ',
-    'SELECT 4, ''Nehemial Kamolu'', ''nkamolu@sandtech.com'', ''126 Happiness  Ave.'''
-);
-
-PREPARE stmt FROM @sql;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES 
+(2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave."),
+(3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave."),
+(4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.");
